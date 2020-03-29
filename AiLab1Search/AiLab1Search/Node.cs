a98fb31a3e69b649b4f675079f311d98cd3821f1
Yura-Androshchuk <yurathebestone@gmail.com>
2020-03-29 22:29:42 +0300

    Display fix

--- AiLab1Search/AiLab1Search/Node.cs
+++ AiLab1Search/AiLab1Search/Node.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 
 namespace AiLab1Search
 {
-    public class Node
+    [Serializable]
+    public class Node //: //ICloneable
     {
         //значення вузла
-        
+ 
         public int[] Value{ get; set; }
         //children
         public List<Node> children;
@@ -18,12 +21,19 @@ namespace AiLab1Search
             this.Value = value;
             Children = new List<Node>();
         }
-        //methods
-        public void AddChild(int[] value)
-        {
-            Children.Add(new Node(value));
-        }
        
+        //methods
+        public void AddChild(Node value)
+        {
+            Children.Add(value);
+        }
+       public Node DeepCopy()
+        {
+            Node temp = (Node)this.MemberwiseClone();
+            temp.Value = this.Value;
+            return temp;
+        }
+        
         //public void PrintPretty(string indent, bool last)
         //{
         //    Console.Write(indent);
@@ -42,5 +52,19 @@ namespace AiLab1Search
         //    for (int i = 0; i < Children.Count; i++)
         //        Children[i].PrintPretty(indent, i == Children.Count - 1);
         //}
+    }
+    public static class ExtensionMethods
+    {
+        // Deep clone
+        public static T DeepClone<T>(this T a)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, a);
+                stream.Position = 0;
+                return (T)formatter.Deserialize(stream);
+            }
+        }
     }
 }
--- AiLab1Search/AiLab1Search/TreeOfStates.cs
+++ AiLab1Search/AiLab1Search/TreeOfStates.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AiLab1Search
@@ -8,7 +9,6 @@ namespace AiLab1Search
     {
         //queue of unchecked states
         Queue<Node> unchecked1 = new Queue<Node>();
-        
         public void CreateTree()
         {
             //root
@@ -16,7 +16,9 @@ namespace AiLab1Search
             //adding root element
             unchecked1.Enqueue(rootElement);
             
-            while (unchecked1.Count > -1)
+
+
+            while (unchecked1.Count > 0)
             {
                 
                 Node current = unchecked1.Dequeue();
@@ -28,50 +30,52 @@ namespace AiLab1Search
                 }
                 Console.WriteLine();
 
+
+                //check1
+
                 Node temp1 = PossibleMoveDoubleLeft(current);
                 if (temp1!=null)
                 {
-                    current.AddChild(temp1.Value);
+                    current.AddChild(temp1);
                 }
+                //check2
                 Node temp2 = PossibleMoveLeft(current);
                 if (temp2 != null)
                 {
-                    current.AddChild(temp2.Value);
+                    current.AddChild(temp2);
                 }
+                //check3
                 Node temp3 = PossibleMoveRight(current);
                 if (temp3 != null)
                 {
-                    current.AddChild(temp3.Value);
+                    current.AddChild(temp3);
                 }
+                //check4
                 Node temp4 = PossibleMoveDoubleRight(current);
                 if (temp4 != null)
                 {
-                    current.AddChild(temp4.Value);
+                    current.AddChild(temp4);
                 }
 
             }
+           int counter =  BFS(rootElement);
+            Console.WriteLine("Nodes Checked");
+            Console.WriteLine(counter);
         }
         // find position of 0 in an array
         public int FindPosZero(int[] a)
         {
-            for (int i = 0; i < 8; i++)
-            {
-                if (a[i] == 0)
-                {
-                    int findPosZero = i;
-                    return findPosZero;
-                }
-
-            }
-            return 0;
+            int zero = 0;
+            int index = Array.IndexOf(a, zero);
+            return index;
             
         }
-        //methods checking the position swappng and adding to a queue
+        //methods checking the position swapping and adding to a queue
         public Node PossibleMoveRight(Node node)
         {
-            
-             int[] tarray = new int[8];
-             tarray = node.Value;
+            Node newcurrent = node.DeepClone();
+            int[] tarray = new int[8];
+            tarray = newcurrent.Value;
             int findPosZero = FindPosZero(tarray);
             if (findPosZero > 0 && tarray[findPosZero - 1] == 2)
             {
@@ -87,9 +91,9 @@ namespace AiLab1Search
         }
         public Node PossibleMoveDoubleRight(Node node)
         {
-           
+            Node newcurrent = node.DeepClone();
             int[] tarray = new int[8];
-            tarray = node.Value;
+            tarray = newcurrent.Value;
             int findPosZero = FindPosZero(tarray);
             if (findPosZero > 1 && tarray[findPosZero - 2] == 2)
             {
@@ -97,15 +101,15 @@ namespace AiLab1Search
                 tarray[findPosZero - 2] = 0;
                 Node newArrayNode = new Node(tarray);
                 unchecked1.Enqueue(newArrayNode);
-                
                 return newArrayNode;
             }
             else { return null; }
         }
         public Node PossibleMoveLeft(Node node)
         {
+            Node newcurrent = node.DeepClone();
             int[] tarray = new int[8];
-            tarray = node.Value;
+            tarray = newcurrent.Value;
             int findPosZero = FindPosZero(tarray);
             if (findPosZero < 7 && tarray[findPosZero + 1] == 1)
             {
@@ -120,9 +124,9 @@ namespace AiLab1Search
         }
         public Node PossibleMoveDoubleLeft(Node node)
         {
-           
+            Node newcurrent = node.DeepClone();
             int[] tarray = new int[8];
-            tarray = node.Value;
+            tarray = newcurrent.Value;
             int findPosZero = FindPosZero(tarray);
             if (findPosZero < 6 &&tarray[findPosZero + 2] == 1 )
             {
@@ -130,13 +134,16 @@ namespace AiLab1Search
                 tarray[findPosZero + 2] = 0;
                 Node newArrayNode = new Node(tarray);
                 unchecked1.Enqueue(newArrayNode);
-                
+         
                 return newArrayNode;
             }
             else { return null; }
         }
-        //add list og children to a queue
-      
+        //compare
+        public bool checkEquality<T>(T[] first, T[] second)
+        {
+            return Enumerable.SequenceEqual(first, second);
+        }
         //bfs shows how many nodes where checked
         public int BFS(Node node)
         {
@@ -149,12 +156,19 @@ namespace AiLab1Search
             {
                 Node currentCheck = checkQueue.Dequeue();
                 counter += 1;
-                if (currentCheck.Value == result.Value)
+                if (checkEquality(result.Value,currentCheck.Value))
                 {
                     ch = false;
+                    return counter;
                 }
                 else
                 {
+                    //Console.WriteLine("---------------------------");
+                    //for (int i = 0; i < 8; i++)
+                    //{
+
+                    //    Console.Write(currentCheck.Value[i]);
+                    //}
                     foreach (Node obj in currentCheck.Children)
                     {
                         checkQueue.Enqueue(obj);
